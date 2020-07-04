92e8a6e57f0f7f240f30e913c3454cdb8c2037bc
Nikita Krivosheyev <jast.e.new.name@gmail.com>
2020-07-04 18:20:27 +0300

    Implemented 1 stage - Addition

--- Numeric Matrix Processor/task/src/processor/Main.kt
+++ Numeric Matrix Processor/task/src/processor/Main.kt
@@ -1,5 +1,34 @@
 package processor
 
+import java.util.*
+
 fun main() {
-    println("Hello World!")
+    val scanner = Scanner(System.`in`)
+
+    val aN = scanner.nextInt()
+    val aM = scanner.nextInt()
+    val sum = Array(aN) { IntArray(aM) }
+
+    for (i in 0 until aN) {
+        for (j in 0 until aM) {
+            sum[i][j] = scanner.nextInt()
+        }
+    }
+
+    val bN = scanner.nextInt()
+    if (aN != bN) {
+        println("ERROR")
+        return
+    }
+    val bM = scanner.nextInt()
+    if (aM != bM) {
+        println("ERROR")
+        return
+    }
+    for (row in sum) {
+        for (elem in row) {
+            print("${elem + scanner.nextInt()} ")
+        }
+        println()
+    }
 }
