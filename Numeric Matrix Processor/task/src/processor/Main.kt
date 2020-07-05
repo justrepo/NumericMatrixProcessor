73835ca4dd09cdec818f69c6fd005eb05499a01b
Nikita Krivosheyev <jast.e.new.name@gmail.com>
2020-07-05 11:29:22 +0300

    Implemented 4 stage - Transpose

--- Numeric Matrix Processor/task/src/processor/Main.kt
+++ Numeric Matrix Processor/task/src/processor/Main.kt
@@ -3,79 +3,145 @@ package processor
 import java.util.*
 
 val scanner = Scanner(System.`in`)
+
+class Matrix(val n: Int, val m: Int) {
+    companion object {
+        fun read(name: String): Matrix {
+            print("Enter size of $name: > ")
+            val n = scanner.nextInt()
+            val m = scanner.nextInt()
+            val result = Matrix(n, m)
+
+            println("Enter $name:")
+            for (i in 0 until n) {
+                print("> ")
+                for (j in 0 until m) {
+                    result.set(i, j, scanner.nextDouble())
+                }
+            }
+
+            return result
+        }
+    }
+
+    private val data = Array(n) { Array(m) { 0.0 } }
+
+    fun at(i: Int, j: Int): Double {
+        return data[i][j]
+    }
+
+    fun set(i: Int, j: Int, value: Double) {
+        data[i][j] = value
+    }
+
+    fun printMatrix(name: String) {
+        println("The $name is :")
+        for (row in data) {
+            for (elem in row) {
+                print("$elem ")
+            }
+            println()
+        }
+    }
+
+    fun plus(second: Matrix): Matrix {
+        if (n != second.n || m != second.m) {
+            println("ERROR")
+            return Matrix(0, 0)
+        }
+
+        val result = Matrix(n, m)
+        for (rowIndex in 0 until n) {
+            for (elementIndex in 0 until m) {
+                result.data[rowIndex][elementIndex] = data[rowIndex][elementIndex] + second.data[rowIndex][elementIndex]
+            }
+        }
+
+        return result
+    }
+
+    fun multiply(second: Double): Matrix {
+        val result = Matrix(n, m)
+
+        for (rowIndex in 0 until n) {
+            for (elementIndex in 0 until m) {
+                result.data[rowIndex][elementIndex] = data[rowIndex][elementIndex] * second
+            }
+        }
+
+        return result
+    }
+
+    fun multiply(second: Matrix): Matrix {
+        if (m != second.n) {
+            println("ERROR")
+            return Matrix(0, 0)
+        }
+
+        val result = Matrix(n, second.m)
+        for (rowIndex in 0 until n) {
+            for (columnIndex in 0 until second.m) {
+                for (elementIndex in 0 until m) {
+                    result.data[rowIndex][columnIndex] += data[rowIndex][elementIndex] * second.data[elementIndex][columnIndex]
+                }
+            }
+        }
+
+        return result
+    }
+
+    fun mainDiagonalTranspose(): Matrix {
+        val result = Matrix(m, n)
+
+        for (i in 0 until n) {
+            for (j in 0 until m) {
+                result.data[j][i] = data[i][j]
+            }
+        }
+
+        return result
+    }
+
+    fun sideDiagonalTranspose(): Matrix {
+        val result = Matrix(m, n)
+
+        for (i in 0 until n) {
+            for (j in 0 until m) {
+                result.data[m - j - 1][n - i - 1] = data[i][j]
+            }
+        }
+
+        return result
+    }
+
+    fun verticalLineTranspose(): Matrix {
+        val result = Matrix(n, m)
+
+        for (i in 0 until n) {
+            for (j in 0 until m) {
+                result.data[i][m - j - 1] = data[i][j]
+            }
+        }
+
+        return result
+    }
+
+    fun horizontalLineTranspose(): Matrix {
+        val result = Matrix(n, m)
+
+        for (i in 0 until n) {
+            for (j in 0 until m) {
+                result.data[n - i - 1][j] = data[i][j]
+            }
+        }
+
+        return result
+    }
+}
+
 fun readDouble(name: String): Double {
     print("Enter $name: > ")
     return scanner.nextDouble()
-}
-
-fun readMatrix(name: String): Array<Array<Double>> {
-    print("Enter size of $name: > ")
-    val n = scanner.nextInt()
-    val m = scanner.nextInt()
-    val result = Array(n) { Array(m) { .0 } }
-
-    println("Enter $name:")
-    for (i in 0 until n) {
-        print("> ")
-        for (j in 0 until m) {
-            result[i][j] = scanner.nextDouble()
-        }
-    }
-
-    return result
-}
-
-fun printMatrix(matrix: Array<Array<Double>>, name: String) {
-    println("The $name is :")
-    for (row in matrix) {
-        for (elem in row) {
-            print("$elem ")
-        }
-        println()
-    }
-}
-
-fun sumMatrices(matrix1: Array<Array<Double>>, matrix2: Array<Array<Double>>): Array<Array<Double>> {
-    if (matrix1.size != matrix2.size) {
-        println("ERROR")
-        return Array(1) { Array(1) { .0 } }
-    }
-
-    for (rowIndex in matrix1.indices) {
-        for (elementIndex in matrix1[rowIndex].indices) {
-            matrix1[rowIndex][elementIndex] += matrix2[rowIndex][elementIndex]
-        }
-    }
-
-    return matrix1
-}
-
-fun multiplyMatrixToAConstant(matrix: Array<Array<Double>>, constant: Double): Array<Array<Double>> {
-    for (rowIndex in matrix.indices) {
-        for (elementIndex in matrix[rowIndex].indices) {
-            matrix[rowIndex][elementIndex] *= constant
-        }
-    }
-
-    return matrix
-}
-
-fun multiplyMatrices(matrix1: Array<Array<Double>>, matrix2: Array<Array<Double>>): Array<Array<Double>> {
-    if (matrix1[0].size != matrix2.size) {
-        println("ERROR")
-        return Array(1) { Array(1) { .0 } }
-    }
-    val result = Array(matrix1.size) { Array(matrix2[0].size) { .0 } }
-
-    for (rowIndex in matrix1.indices) {
-        for (columnIndex in matrix2[0].indices) {
-            for (elementIndex in matrix1[rowIndex].indices) {
-                result[rowIndex][columnIndex] += matrix1[rowIndex][elementIndex] * matrix2[elementIndex][columnIndex]
-            }
-        }
-    }
-
-    return result
 }
 
 fun main() {
@@ -83,13 +149,27 @@ fun main() {
         print("1. Add matrices\n" +
                 "2. Multiply matrix to a constant\n" +
                 "3. Multiply matrices\n" +
+                "4. Transpose matrix\n" +
                 "0. Exit\n" +
                 "Your choice: > ")
         val choice = scanner.nextInt()
         when (choice) {
-            1 -> printMatrix(sumMatrices(readMatrix("first matrix"), readMatrix("second matrix")), "sum")
-            2 -> printMatrix(multiplyMatrixToAConstant(readMatrix("matrix"), readDouble("constant")), "multiplication result")
-            3 -> printMatrix(multiplyMatrices(readMatrix("first matrix"), readMatrix("second matrix")), "multiplication result")
+            1 -> Matrix.read("first matrix").plus(Matrix.read("second matrix")).printMatrix("sum")
+            2 -> Matrix.read("matrix").multiply(readDouble("constant")).printMatrix("multiplication result")
+            3 -> Matrix.read("first matrix").multiply(Matrix.read("second matrix")).printMatrix("multiplication result")
+            4 -> {
+                print("1. Main diagonal\n" +
+                        "2. Side diagonal\n" +
+                        "3. Vertical line\n" +
+                        "4. Horizontal line\n" +
+                        "Your choice: > ")
+                when (scanner.nextInt()) {
+                    1 -> Matrix.read("matrix").mainDiagonalTranspose().printMatrix("result")
+                    2 -> Matrix.read("matrix").sideDiagonalTranspose().printMatrix("result")
+                    3 -> Matrix.read("matrix").verticalLineTranspose().printMatrix("result")
+                    4 -> Matrix.read("matrix").horizontalLineTranspose().printMatrix("result")
+                }
+            }
         }
     } while (choice != 0)
 }
